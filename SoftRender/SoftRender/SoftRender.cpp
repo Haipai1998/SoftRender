f9c19693e89a78830a1230ddd3933c5d69f864d5
shuhaipeng <shuhaipeng@tencent.com>
2019-08-01 16:32:12 +0800

    旋转后出现BUG，待解决

--- SoftRender/SoftRender/SoftRender.cpp
+++ SoftRender/SoftRender/SoftRender.cpp
@@ -42,6 +42,9 @@ typedef struct {
     float x, y, z, w;
 }point_t, vector_t;
 
+float camera_pos = 3.0f;
+float camera_theta = 1.0f;
+
 int screen_exit;
 int screen_pitch;
 int screen_w, screen_h;
@@ -69,8 +72,34 @@ static LRESULT screen_events(HWND hWnd, UINT msg,
     WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_CLOSE: screen_exit = 1; break;
-    //case WM_KEYDOWN: screen_keys[wParam & 511] = 1; break;
-    //case WM_KEYUP: screen_keys[wParam & 511] = 0; break;
+    case WM_KEYDOWN: screen_keys[wParam & 511] = 1; break;
+    case WM_KEYUP: screen_keys[wParam & 511] = 0; break;
+    //case WM_KEYDOWN: {
+    //    switch (wParam)
+    //    {
+    //    case VK_ESCAPE: {
+    //        screen_exit = 1;
+    //        break;
+    //    }
+    //    case VK_LEFT: {
+    //        camera_theta += 0.01f; break;
+    //    }
+    //    case VK_RIGHT: {
+    //        camera_theta -= 0.01f; break;
+    //    }
+    //    case VK_UP: {
+    //        //printf("VK UP %f\n", camera_pos);
+    //        camera_pos -= 0.01f; break;
+    //    }
+    //    case VK_DOWN: {
+    //        //printf("VK DOWN %f\n", camera_pos);
+    //        camera_pos += 0.01f; break;
+    //    }
+
+    //    default:
+    //        break;
+    //    }
+    //}
     default: return DefWindowProc(hWnd, msg, wParam, lParam);
     }
     return 0;
@@ -234,7 +263,6 @@ void device_InitFB(device_t *device) {
     return;
 }
 
-
 void screen_update() {
     HDC hDC = GetDC(screen_handle);
     BitBlt(hDC, 0, 0, screen_w, screen_h, screen_dc, 0, 0, SRCCOPY);
@@ -328,7 +356,7 @@ void matrix_set_lookat(matrix_t *m, const vector_t *eye, const vector_t *at, con
 }
 
 //根据新的相机位置设立view矩阵
-void camera_set_pos(device_t *device, int x, int y, int z) {
+void camera_set_pos(device_t *device, float x, float y, float z) {
     point_t eye = { x, y, z, 1 }, at = { 0, 0, 0, 1 }, up = { 0, 0, 1, 1 };
     matrix_set_lookat(&device->transform.view, &eye, &at, &up);//重新更新世界到相机空间的矩阵
     transform_update(&device->transform);
@@ -358,16 +386,18 @@ void matrix_set_rotate(matrix_t *m, float x, float y, float z, float theta) {
     m->m[3][0] = m->m[3][1] = m->m[3][2] = 0.0f;
     m->m[3][3] = 1.0f;
 }
-typedef struct { point_t pos; float rhw; } vertex_t;
+typedef struct { float r, g, b; } color_t;
+typedef struct { float u, v; } texcoord_t;
+typedef struct { point_t pos; texcoord_t tc; color_t color; float rhw; } vertex_t;
 vertex_t mesh[8] = {
-    { { -1, -1,  1, 1 }, 1 },
-    { {  1, -1,  1, 1 }, 1 },
-    { {  1,  1,  1, 1 }, 1 },
-    { { -1,  1,  1, 1 }, 1 },
-    { { -1, -1, -1, 1 }, 1 },
-    { {  1, -1, -1, 1 }, 1 },
-    { {  1,  1, -1, 1 }, 1 },
-    { { -1,  1, -1, 1 }, 1 },
+    { { -1, -1,  1, 1 }, { 0, 0 }, { 1.0f, 0.2f, 0.2f }, 1 },
+    { {  1, -1,  1, 1 }, { 0, 1 }, { 0.2f, 1.0f, 0.2f }, 1 },
+    { {  1,  1,  1, 1 }, { 1, 1 }, { 0.2f, 0.2f, 1.0f }, 1 },
+    { { -1,  1,  1, 1 }, { 1, 0 }, { 1.0f, 0.2f, 1.0f }, 1 },
+    { { -1, -1, -1, 1 }, { 0, 0 }, { 1.0f, 1.0f, 0.2f }, 1 },
+    { {  1, -1, -1, 1 }, { 0, 1 }, { 0.2f, 1.0f, 1.0f }, 1 },
+    { {  1,  1, -1, 1 }, { 1, 1 }, { 1.0f, 0.3f, 0.3f }, 1 },
+    { { -1,  1, -1, 1 }, { 1, 0 }, { 0.2f, 1.0f, 0.3f }, 1 },
 };
 void device_pixel(device_t *device, int x, int y, UINT32 color) {
     if (((UINT32)x) < (UINT32)device->width && ((UINT32)y) < (UINT32)device->height) {
@@ -438,6 +468,214 @@ void transform_homogenize(const transform_t *ts, vector_t *y, const vector_t *x)
     y->z = x->z * rhw;
     y->w = 1.0f;
 }
+
+typedef struct { vertex_t v, step; int x, y, w; } scanline_t;
+typedef struct { vertex_t v, v1, v2; } edge_t;
+typedef struct { float top, bottom; edge_t left, right; } trapezoid_t;
+
+void vertex_rhw_init(vertex_t *v) {
+    float rhw = 1.0f / v->pos.w;
+    v->rhw = rhw;
+    v->tc.u *= rhw;
+    v->tc.v *= rhw;
+    v->color.r *= rhw;
+    v->color.g *= rhw;
+    v->color.b *= rhw;
+}
+int trapezoid_init_triangle(trapezoid_t *trap, const vertex_t *p1,
+    const vertex_t *p2, const vertex_t *p3) {
+    const vertex_t *p;
+    float k, x;
+
+    if (p1->pos.y > p2->pos.y) p = p1, p1 = p2, p2 = p;
+    if (p1->pos.y > p3->pos.y) p = p1, p1 = p3, p3 = p;
+    if (p2->pos.y > p3->pos.y) p = p2, p2 = p3, p3 = p;
+    if (p1->pos.y == p2->pos.y && p1->pos.y == p3->pos.y) return 0;
+    if (p1->pos.x == p2->pos.x && p1->pos.x == p3->pos.x) return 0;
+
+    if (p1->pos.y == p2->pos.y) {	// triangle down
+        if (p1->pos.x > p2->pos.x) p = p1, p1 = p2, p2 = p;
+        trap[0].top = p1->pos.y;
+        trap[0].bottom = p3->pos.y;
+        trap[0].left.v1 = *p1;
+        trap[0].left.v2 = *p3;
+        trap[0].right.v1 = *p2;
+        trap[0].right.v2 = *p3;
+        return (trap[0].top < trap[0].bottom) ? 1 : 0;
+    }
+
+    if (p2->pos.y == p3->pos.y) {	// triangle up
+        if (p2->pos.x > p3->pos.x) p = p2, p2 = p3, p3 = p;
+        trap[0].top = p1->pos.y;
+        trap[0].bottom = p3->pos.y;
+        trap[0].left.v1 = *p1;
+        trap[0].left.v2 = *p2;
+        trap[0].right.v1 = *p1;
+        trap[0].right.v2 = *p3;
+        return (trap[0].top < trap[0].bottom) ? 1 : 0;
+    }
+
+    trap[0].top = p1->pos.y;
+    trap[0].bottom = p2->pos.y;
+    trap[1].top = p2->pos.y;
+    trap[1].bottom = p3->pos.y;
+
+    k = (p3->pos.y - p1->pos.y) / (p2->pos.y - p1->pos.y);
+    x = p1->pos.x + (p2->pos.x - p1->pos.x) * k;
+
+    if (x <= p3->pos.x) {		// triangle left
+        trap[0].left.v1 = *p1;
+        trap[0].left.v2 = *p2;
+        trap[0].right.v1 = *p1;
+        trap[0].right.v2 = *p3;
+        trap[1].left.v1 = *p2;
+        trap[1].left.v2 = *p3;
+        trap[1].right.v1 = *p1;
+        trap[1].right.v2 = *p3;
+    }
+    else {					// triangle right
+        trap[0].left.v1 = *p1;
+        trap[0].left.v2 = *p3;
+        trap[0].right.v1 = *p1;
+        trap[0].right.v2 = *p2;
+        trap[1].left.v1 = *p1;
+        trap[1].left.v2 = *p3;
+        trap[1].right.v1 = *p2;
+        trap[1].right.v2 = *p3;
+    }
+
+    return 2;
+}
+
+int CMID(int x, int min, int max) { return (x < min) ? min : ((x > max) ? max : x); }
+
+// 根据坐标读取纹理
+UINT32 device_texture_read(const device_t *device, float u, float v) {
+    int x, y;
+    u = u * device->max_u;
+    v = v * device->max_v;
+    x = (int)(u + 0.5f);
+    y = (int)(v + 0.5f);
+    x = CMID(x, 0, device->tex_width - 1);
+    y = CMID(y, 0, device->tex_height - 1);
+    return device->texture[y][x];
+}
+void vertex_add(vertex_t *y, const vertex_t *x) {
+    y->pos.x += x->pos.x;
+    y->pos.y += x->pos.y;
+    y->pos.z += x->pos.z;
+    y->pos.w += x->pos.w;
+    y->rhw += x->rhw;
+    y->tc.u += x->tc.u;
+    y->tc.v += x->tc.v;
+    y->color.r += x->color.r;
+    y->color.g += x->color.g;
+    y->color.b += x->color.b;
+}
+// 绘制扫描线
+void device_draw_scanline(device_t *device, scanline_t *scanline) {
+    UINT32 *framebuffer = device->framebuffer[scanline->y];
+    float *zbuffer = device->zbuffer[scanline->y];
+    int x = scanline->x;
+    int w = scanline->w;
+    int width = device->width;
+    int render_state = device->render_state;
+    for (; w > 0; x++, w--) {
+        if (x >= 0 && x < width) {
+            float rhw = scanline->v.rhw;
+            if (rhw >= zbuffer[x]) {
+                float w = 1.0f / rhw;
+                zbuffer[x] = rhw;
+                if (render_state & RENDER_STATE_COLOR) {
+                    float r = scanline->v.color.r * w;
+                    float g = scanline->v.color.g * w;
+                    float b = scanline->v.color.b * w;
+                    int R = (int)(r * 255.0f);
+                    int G = (int)(g * 255.0f);
+                    int B = (int)(b * 255.0f);
+                    R = CMID(R, 0, 255);
+                    G = CMID(G, 0, 255);
+                    B = CMID(B, 0, 255);
+                    framebuffer[x] = (R << 16) | (G << 8) | (B);
+                }
+                if (render_state & RENDER_STATE_TEXTURE) {
+                    float u = scanline->v.tc.u * w;
+                    float v = scanline->v.tc.v * w;
+                    UINT32 cc = device_texture_read(device, u, v);
+                    framebuffer[x] = cc;
+                }
+            }
+        }
+        vertex_add(&scanline->v, &scanline->step);
+        if (x >= width) break;
+    }
+}
+void vertex_division(vertex_t *y, const vertex_t *x1, const vertex_t *x2, float w) {
+    float inv = 1.0f / w;
+    y->pos.x = (x2->pos.x - x1->pos.x) * inv;
+    y->pos.y = (x2->pos.y - x1->pos.y) * inv;
+    y->pos.z = (x2->pos.z - x1->pos.z) * inv;
+    y->pos.w = (x2->pos.w - x1->pos.w) * inv;
+    y->tc.u = (x2->tc.u - x1->tc.u) * inv;
+    y->tc.v = (x2->tc.v - x1->tc.v) * inv;
+    y->color.r = (x2->color.r - x1->color.r) * inv;
+    y->color.g = (x2->color.g - x1->color.g) * inv;
+    y->color.b = (x2->color.b - x1->color.b) * inv;
+    y->rhw = (x2->rhw - x1->rhw) * inv;
+}
+// 根据左右两边的端点，初始化计算出扫描线的起点和步长
+void trapezoid_init_scan_line(const trapezoid_t *trap, scanline_t *scanline, int y) {
+    float width = trap->right.v.pos.x - trap->left.v.pos.x;
+    scanline->x = (int)(trap->left.v.pos.x + 0.5f);
+    scanline->w = (int)(trap->right.v.pos.x + 0.5f) - scanline->x;
+    scanline->y = y;
+    scanline->v = trap->left.v;
+    if (trap->left.v.pos.x >= trap->right.v.pos.x) scanline->w = 0;
+    vertex_division(&scanline->step, &trap->left.v, &trap->right.v, width);
+}
+// 计算插值：t 为 [0, 1] 之间的数值
+float interp(float x1, float x2, float t) { return x1 + (x2 - x1) * t; }
+void vector_interp(vector_t *z, const vector_t *x1, const vector_t *x2, float t) {
+    z->x = interp(x1->x, x2->x, t);
+    z->y = interp(x1->y, x2->y, t);
+    z->z = interp(x1->z, x2->z, t);
+    z->w = 1.0f;
+}
+
+void vertex_interp(vertex_t *y, const vertex_t *x1, const vertex_t *x2, float t) {
+    vector_interp(&y->pos, &x1->pos, &x2->pos, t);
+    y->tc.u = interp(x1->tc.u, x2->tc.u, t);
+    y->tc.v = interp(x1->tc.v, x2->tc.v, t);
+    y->color.r = interp(x1->color.r, x2->color.r, t);
+    y->color.g = interp(x1->color.g, x2->color.g, t);
+    y->color.b = interp(x1->color.b, x2->color.b, t);
+    y->rhw = interp(x1->rhw, x2->rhw, t);
+}
+// 按照 Y 坐标计算出左右两条边纵坐标等于 Y 的顶点
+void trapezoid_edge_interp(trapezoid_t *trap, float y) {
+    float s1 = trap->left.v2.pos.y - trap->left.v1.pos.y;
+    float s2 = trap->right.v2.pos.y - trap->right.v1.pos.y;
+    float t1 = (y - trap->left.v1.pos.y) / s1;
+    float t2 = (y - trap->right.v1.pos.y) / s2;
+    vertex_interp(&trap->left.v, &trap->left.v1, &trap->left.v2, t1);
+    vertex_interp(&trap->right.v, &trap->right.v1, &trap->right.v2, t2);
+}
+// 主渲染函数
+void device_render_trap(device_t *device, trapezoid_t *trap) {
+    scanline_t scanline;
+    int j, top, bottom;
+    top = (int)(trap->top + 0.5f);
+    bottom = (int)(trap->bottom + 0.5f);
+    for (j = top; j < bottom; j++) {
+        if (j >= 0 && j < device->height) {
+            trapezoid_edge_interp(trap, (float)j + 0.5f);
+            trapezoid_init_scan_line(trap, &scanline, j);
+            device_draw_scanline(device, &scanline);
+        }
+        if (j >= device->height) break;
+    }
+}
+
 void device_draw_primitive(device_t *device, const vertex_t *v1,
     const vertex_t *v2, const vertex_t *v3) {
     point_t p1, p2, p3, c1, c2, c3;
@@ -453,6 +691,32 @@ void device_draw_primitive(device_t *device, const vertex_t *v1,
     transform_homogenize(&device->transform, &p2, &c2);
     transform_homogenize(&device->transform, &p3, &c3);
 
+    if (render_state & (RENDER_STATE_TEXTURE | RENDER_STATE_COLOR)) {
+        vertex_t t1 = *v1, t2 = *v2, t3 = *v3;
+        trapezoid_t traps[2];
+        int n;
+
+        t1.pos = p1;
+        t2.pos = p2;
+        t3.pos = p3;
+        t1.pos.w = c1.w;
+        t2.pos.w = c2.w;
+        t3.pos.w = c3.w;
+
+        vertex_rhw_init(&t1);	// 初始化 w
+        vertex_rhw_init(&t2);	// 初始化 w
+        vertex_rhw_init(&t3);	// 初始化 w
+
+        // 拆分三角形为0-2个梯形，并且返回可用梯形数量
+        n = trapezoid_init_triangle(traps, &t1, &t2, &t3);
+
+        if (n == 1) device_render_trap(device, &traps[0]);
+        else if (n == 2) {
+            device_render_trap(device, &traps[0]);
+            device_render_trap(device, &traps[1]);
+        }
+    }
+
     if (render_state & RENDER_STATE_WIREFRAME) {		// 线框绘制
         device_draw_line(device, (int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, device->foreground);
         device_draw_line(device, (int)p1.x, (int)p1.y, (int)p3.x, (int)p3.y, device->foreground);
@@ -462,6 +726,9 @@ void device_draw_primitive(device_t *device, const vertex_t *v1,
 
 void draw_plane(device_t *device, int a, int b, int c, int d) {
     vertex_t p1 = mesh[a], p2 = mesh[b], p3 = mesh[c], p4 = mesh[d];
+    //给每个顶点加u,v
+    p1.tc.u = 0, p1.tc.v = 0, p2.tc.u = 0, p2.tc.v = 1;
+    p3.tc.u = 1, p3.tc.v = 1, p4.tc.u = 1, p4.tc.v = 0;
     device_draw_primitive(device, &p1, &p2, &p3);
     device_draw_primitive(device, &p3, &p4, &p1);
 }
@@ -478,15 +745,36 @@ void Draw_Box(device_t *device, float theta) {
     draw_plane(device, 2, 6, 7, 3);
     draw_plane(device, 3, 7, 4, 0);
 }
+// 设置当前纹理
+void device_set_texture(device_t *device, void *bits, long pitch, int w, int h) {
+    UINT32 *ptr = (UINT32*)bits;
+    int j;
+    for (j = 0; j < h; ptr += pitch, j++) 	// 重新计算每行纹理的指针
+        device->texture[j] = (UINT32*)ptr;
+    device->tex_width = w;
+    device->tex_height = h;
+    device->max_u = (float)(w - 1);
+    device->max_v = (float)(h - 1);
+}
+void texture_init(device_t *device) {
+    static UINT32 texture[256][256];
+    int i, j;
+    for (j = 0; j < 256; j++) {
+        for (i = 0; i < 256; i++) {
+            int x = i / 32, y = j / 32;
+            texture[j][i] = ((x + y) & 1) ? 0xffffff : 0x3fbcef;
+        }
+    }
+    device_set_texture(device, texture, 256, 256, 256);
+}
 
 int main(void) {
     device_t device; //定义渲染方块结构体，包括背景颜色，框大小
 
-    const TCHAR *Title = _T("Soft Render implement");
+    const TCHAR *Title = _T("Soft Render Demo");
     screen_w = 800;
     screen_h = 600;
-    float camera_pos = 3.0f;
-    float camera_theta = 1.0f;
+
     int res = screen_init(screen_w, screen_h, Title); //初始化窗口
     if (!res) {
         //ok maybe dosth to celebrate successful?
@@ -497,26 +785,58 @@ int main(void) {
     }
 
     device_init(&device, screen_w, screen_h,screen_fb);// 初始化设备，将HDC和对应的内存绑定
-    
+    texture_init(&device);
     //printf(">>!%d", strlen((const char*)screen_fb));
     //for (int i = 0; i < strlen((const char*)screen_fb);++i) {
     //    printf("%c ! ",screen_fb[i]);
     //}
     //printf(">>");
-    
+    int cnt = 0;
     while (true) {
         if (screen_exit)    break;
-
-        screen_dispatch();//分发所有命令给回调函数去处理：主要处理按键输入
+        if (screen_keys[VK_ESCAPE])  break;
         device_InitFB(&device);
+        screen_dispatch();//分发所有命令给回调函数去处理：主要处理按键输入
 
         camera_set_pos(&device, 0, camera_pos, 0);
-
+        //printf("Reset POS %f\n",camera_pos);
         //画线，画点测试
         //for (int i = 0; i<=50; i++) {
         //    DrawPoint(&device, 400-i, 300-i, ((200+i) << 16) | (200 + i) | ((200 + i) << 8));
         //    printf("i=%d\n",i);
         //}
+
+        if (screen_keys[VK_UP]) camera_pos -= 0.01f;
+        if (screen_keys[VK_DOWN]) camera_pos += 0.01f;
+        if (screen_keys[VK_LEFT]) camera_theta += 0.01f;
+        if (screen_keys[VK_RIGHT]) camera_theta -= 0.01f;
+
+        //这样处理会闪烁,可能在UP之前发出多个DOWN的消息，导致while被跑了好多次
+        //if (screen_keys[VK_SPACE]) {
+        //    if (device.render_state & RENDER_STATE_COLOR) {
+        //        device.render_state = RENDER_STATE_WIREFRAME;
+        //    }
+        //    else {
+        //        device.render_state *= 2;
+        //    }
+        //}
+
+        if (screen_keys[VK_SPACE])
+        {
+            if (cnt == 0) {
+                cnt++;
+                if (device.render_state & RENDER_STATE_COLOR)
+                {
+                    device.render_state = RENDER_STATE_WIREFRAME;
+                }
+                else {
+                    device.render_state *= 2;
+                }
+            }
+        }
+        else {
+            cnt = 0;
+        }
 
         Draw_Box(&device, camera_theta);
 
